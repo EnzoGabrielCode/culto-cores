aec9bf7b6098aa0030a22211184db875bd403a3c
Enzo Gabriel <enzokof2005@gmail.com>
2025-11-07 14:45:05 -0300

    feat: botao sorteio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const sortearBtn = document.getElementById("sortear-btn");
   const imgElement = document.getElementById("versiculo-img");
 
-  if (imgElement) {
-    const randomNumber = Math.floor(Math.random() * 7) + 1;
-    const imagePath = `./assets/img/versiculos/${randomNumber}.png`;
+  if (sortearBtn) {
+    sortearBtn.addEventListener("click", function () {
+      const randomNumber = Math.floor(Math.random() * 7) + 1;
+      const imagePath = `./assets/img/versiculos/${randomNumber}.png`;
+      imgElement.src = imagePath;
 
-    imgElement.src = imagePath;
+      document.body.classList.remove("antes-sorteio");
+      document.body.classList.add("depois-sorteio");
+    });
   }
 });
